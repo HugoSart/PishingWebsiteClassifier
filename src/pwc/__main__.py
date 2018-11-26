94ddf7558df8bbf1b6a8ca71fb120824ae6bdaea
Hugo Sartori <hugo_sart@hotmail.com>
2018-11-26 21:23:27 -0200

    Terminado

--- src/pwc/__main__.py
+++ src/pwc/__main__.py
@@ -7,6 +7,7 @@ def run_test(classifier, test_dataset):
 
     hit_safe = miss_safe = hit_phishing = miss_phishing = 0
 
+    total = total_safe = total_phishing = 0
     for site in test_dataset:
         c = classifier.classify(site)
         if site.attributes[Website.Tag.RESULT.value] == c:
@@ -20,13 +21,24 @@ def run_test(classifier, test_dataset):
             else:
                 miss_safe += 1
 
-    print("+----------+----------+----------+")
-    print("|          | SAFE     | PHISHING |")
-    print("+----------+----------+----------+")
-    print("| SAFE     | %4d     | %4d     |" % (hit_safe, miss_safe))
-    print("+----------+----------+----------+")
-    print("| PHISHING | %4d     | %4d     |" % (miss_safe, hit_phishing))
-    print("+----------+----------+----------+")
+    total_safe = hit_safe + miss_phishing
+    total_phishing = hit_phishing + miss_safe
+    total = total_safe + total_phishing
+
+    precision = (hit_safe / (hit_safe + miss_safe)) * 100
+    recall = (hit_safe / (hit_safe + miss_phishing)) * 100
+    f = 2 * ((precision * recall) / (precision + recall))
+    accuracy = ((hit_safe + hit_phishing) / total) * 100
+
+    print("\n======================== TEST RESULTS ========================\n")
+    print("  +----------+----------+----------+    Precision  : %4.2f %%" % precision)
+    print("  |          | SAFE     | PHISHING |")
+    print("  +----------+----------+----------+    Recall     : %4.2f %%" % recall)
+    print("  | SAFE     | %4d     | %4d     |" % (hit_safe, miss_safe))
+    print("  +----------+----------+----------+    F-Measure  : %4.2f %%" % f)
+    print("  | PHISHING | %4d     | %4d     |" % (miss_phishing, hit_phishing))
+    print("  +----------+----------+----------+    Accuracy   : %4.2f %%" % accuracy)
+    print("\n==============================================================")
 
 
 def __main__():
